da89d6a7afd098394d148f5c4eb0f1953b05f75f
dhleong <falcone88@gmail.com>
2015-11-20 13:52:30 -0500

    Clean up leiningen profile to be less gross

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,4 @@
-{:user {
-        :plugins [
-                  [cider/cider-nrepl "0.7.0"]
-                  [lein-kibit "0.0.8"]
-                  ]}}
+{:user 
+ {:plugins [[cider/cider-nrepl "0.7.0"]
+            [lein-kibit "0.0.8"]
+            ]}}
