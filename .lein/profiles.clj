e5dfc32399d658fe66ec431d1b2c617ba8e25df6
Daniel Leong <me@dhleong.net>
2018-08-08 08:13:23 -0400

    Add lein-ancient plugin globally

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
-{:user 
+{:user
  {:plugins [[cider/cider-nrepl "0.16.0"]
+            [lein-ancient "0.6.15"]
             [lein-kibit "0.0.8"]]}
  :android-user {:dependencies [[cider/cider-nrepl "0.9.1"]]
                 :android {:aot-exclude-ns ["cider.nrepl.middleware.util.java.parser"
