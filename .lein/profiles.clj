734a7a0987844f42d580159120f3fecf8b3c6c51
Daniel Leong <me@dhleong.net>
2019-07-09 09:24:36 -0400

    Add refactor-nrepl middleware

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,10 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.21.1"]
+ {:plugins [[refactor-nrepl "2.4.0"]
+            [cider/cider-nrepl "0.21.1"]
+
             [lein-ancient "0.6.15"]
             [lein-kibit "0.1.6"]]}
+ :repl-options {:nrepl-middleware [refactor-nrepl.middleware/wrap-refactor]}
  :android-user {:dependencies [[cider/cider-nrepl "0.9.1"]]
                 :android {:aot-exclude-ns ["cider.nrepl.middleware.util.java.parser"
                                            "cider.nrepl" "cider-nrepl.plugin"]}}
