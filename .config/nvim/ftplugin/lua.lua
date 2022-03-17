1c1734d11e7546a4842776a291e17dbc40d9cb14
Daniel Leong <me@dhleong.net>
2022-03-17 13:32:48 +0000

    nvim: Reorganize ftplugin/lua + fix leaky sw/ts settings

--- .config/nvim/ftplugin/lua.lua
+++ .config/nvim/ftplugin/lua.lua
@@ -1,15 +1,24 @@
-vim.o.shiftwidth = 2
-vim.o.tabstop = 2
-
 require('helpers.lsp').config('sumneko_lua', {
   settings = {
     Lua = {
       diagnostics = {
         globals = { 'vim' },
       },
+      telemetry = {
+        -- Disable sending (anonymized) telemetry
+        enable = false,
+      },
     },
   },
 })
+
+-- ======= Settings =======================================
+
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+
+
+-- ======= Mappings =======================================
 
 vim.cmd [[
   nnoremap <buffer> K <cmd>lua require'ft.lua'.doc()<cr>
