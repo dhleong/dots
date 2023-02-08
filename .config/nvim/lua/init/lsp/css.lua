e8fa036f815d2d546c2ba068b625abec8ac2a689
Daniel Leong <me@dhleong.net>
2023-02-08 11:07:54 -0500

    Port cssmodules config from main to fix conflict with tsserver `gd`

--- .config/nvim/lua/init/lsp/css.lua
+++ .config/nvim/lua/init/lsp/css.lua
@@ -1,1 +1,6 @@
-require('helpers.lsp').config('cssmodules_ls')
+require('helpers.lsp').config('cssmodules_ls', {
+  on_attach = function(client)
+    -- Avoid conflicts with tsserver go-to-definition
+    client.server_capabilities.definitionProvider = false
+  end
+})
