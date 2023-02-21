23dbdddeab8336799cd3e2e881540d18b2dd2c4b
Daniel Leong <me@dhleong.net>
2023-02-21 13:32:42 -0500

    nvim: Ensure jsonls validation is enabled

--- .config/nvim/lua/init/lsp/json.lua
+++ .config/nvim/lua/init/lsp/json.lua
@@ -2,6 +2,12 @@ require('helpers.lsp').config('jsonls', {
   settings = {
     json = {
       schemas = require('schemastore').json.schemas(),
+      format = {
+        enable = false,
+      },
+      validate = {
+        enable = true,
+      },
     },
   },
 })
