6b76ba513549efe6a062c8c18517c9d6b3aa0cf0
Daniel Leong <me@dhleong.net>
2023-03-07 19:41:15 +0000

    lua: Disable 3rd party library checks
    Those were super annoying and kept popping up even though I constantly
    said "don't ask again"

--- .config/nvim/lua/init/lsp/lua.lua
+++ .config/nvim/lua/init/lsp/lua.lua
@@ -6,6 +6,9 @@ require('helpers.lsp').config('lua_ls', {
         version = 'LuaJIT',
       },
       workspace = {
+        -- Disable third party library checks (super annoying)
+        checkThirdParty = false,
+
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
       },
