b285c57bc96f4a96ce9b8e1ffca217fe7f1d463c
Daniel Leong <me@dhleong.net>
2023-10-22 10:48:39 -0400

    Add a new "monorepo" mode for projects to make navigation faster
    When opening a file that's part of a monorepo (as we've defined it, a
    project whose directory name prefixes the name of subdirectory
    containing the selected file, separated by `-` or `_`) we now use that
    sub-directory (sub-project) as the root of the "project" for all
    project navigation mappings, and for the local `path`. This makes it
    much faster to navigate within the sub-project, which is much more
    common than navigating across sub-projects.
    
    On the rare occasion I need cross-monorepo file navigation, it can
    still accessed under the <leader>m prefix

--- .config/nvim/lua/dhleong/projects.lua
+++ .config/nvim/lua/dhleong/projects.lua
@@ -16,7 +16,7 @@ parent_paths = vim.tbl_map(function(path)
   return vim.env.HOME .. path
 end, parent_paths)
 
-local function create_project_navigation_maps(project_dir)
+local function create_project_navigation_maps(paths)
   local function nmap(lhs, nav_call)
     local lua_call = "require'dhleong.nav'." .. nav_call
     vim.api.nvim_buf_set_keymap(0, 'n', lhs, '<cmd>lua ' .. lua_call .. '<cr>', {
@@ -25,6 +25,7 @@ local function create_project_navigation_maps(project_dir)
     })
   end
 
+  local project_dir = paths.project_dir
   local project_path = 'nil'
   if project_dir then
     project_path = '"' .. project_dir .. '"'
@@ -38,12 +39,50 @@ local function create_project_navigation_maps(project_dir)
   nmap('<c-p>', "in_project(" .. project_path .. ", 'e')")
   nmap('<c-w><c-p>', "in_project(" .. project_path .. ", 'tabe')")
   nmap('<c-s><c-p>', "in_project(" .. project_path .. ", 'vsplit')")
-  -- TODO in_project_subpath
   nmap('\\', "by_text(" .. project_path .. ", 'e')")
   nmap('|', "resume_by_text(" .. project_path .. ", 'e')")
   nmap('<c-w>\\', "by_text(" .. project_path .. ", 'tabe')")
   nmap('<c-s>\\', "by_text(" .. project_path .. ", 'tabe')")
   nmap('g\\', "by_text(" .. project_path .. ", 'e', { query = vim.fn.expand('<cword>') })")
+
+  if paths.monorepo_root then
+    nmap('<leader>m\\', "by_text('" .. paths.monorepo_root .. "', 'e')")
+    nmap('<leader>m|', "resume_by_text('" .. paths.monorepo_root .. "', 'e')")
+    nmap('<leader>mg\\', "by_text(" .. paths.monorepo_root .. ", 'e', { query = vim.fn.expand('<cword>') })")
+    nmap('<leader>m<c-p>', "in_project('" .. paths.monorepo_root .. "', 'e')")
+  end
+end
+
+local function extract_project_paths(proj_dir, file_in_dir, idx)
+  -- Build the path
+  local proj_name = file_in_dir:sub(2, idx) -- skip past the /
+  local path_dir = proj_dir .. proj_name
+
+  -- Is this a simple monorepo? These look like eg:
+  -- project/project_subproject
+  -- NOTE: It might be nice to have some sort of heuristic for "project size"
+  -- at which this is useful...
+  local subproj_idx = file_in_dir:find('/', idx + 1)
+  if subproj_idx then
+    local subproj_name = file_in_dir:sub(idx + 1, subproj_idx)
+
+    -- Trim the trailing slashes
+    local clean_subproj_name = vim.fn.trim(subproj_name, '/', 2)
+    local clean_proj_name = vim.fn.trim(proj_name, '/', 2)
+
+    for _, separator in ipairs { '_', '-' } do
+      if vim.startswith(clean_subproj_name, clean_proj_name .. separator) then
+        return {
+          monorepo_root = path_dir,
+          project_dir = path_dir .. subproj_name
+        }
+      end
+    end
+  end
+
+  return {
+    project_dir = path_dir,
+  }
 end
 
 local function configure_buffer()
@@ -65,12 +104,11 @@ local function configure_buffer()
       end
 
       -- Build the path
-      local proj_name = file_in_dir:sub(2, idx) -- skip past the /
-      local path_dir = proj_dir .. proj_name
+      local paths = extract_project_paths(proj_dir, file_in_dir, idx)
 
       -- Set it!
-      vim.bo.path = path_dir .. '**'
-      create_project_navigation_maps(path_dir)
+      vim.bo.path = paths.project_dir .. '**'
+      create_project_navigation_maps(paths)
       return
     end
 
@@ -78,7 +116,9 @@ local function configure_buffer()
   end
 
   -- If we got here, we found no path
-  create_project_navigation_maps(nil)
+  create_project_navigation_maps {
+    project_dir = nil
+  }
 end
 
 local Projects = {
