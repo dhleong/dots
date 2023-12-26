b377031870748eacf283757f4a785d2d8b29383c
Daniel Leong <me@dhleong.net>
2023-12-26 16:09:33 -0500

    Actually apply the cmp-under-comparator

--- .config/nvim/lua/init/lsp.lua
+++ .config/nvim/lua/init/lsp.lua
@@ -71,6 +71,7 @@ cmp.setup({
       cmp.config.compare.offset,
       cmp.config.compare.exact,
       cmp.config.compare.score,
+      require 'cmp-under-comparator'.under,
       cmp.config.compare.recently_used,
       cmp.config.compare.kind,
       cmp.config.compare.sort_text,
