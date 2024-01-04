d69e2794899e9d6029ba60d12ad4b30f9d4b1379
Daniel Leong <me@dhleong.net>
2024-01-04 14:09:45 -0500

    Add lua profiling via plenary to our Profile command

--- .config/nvim/lua/init/commands.lua
+++ .config/nvim/lua/init/commands.lua
@@ -7,17 +7,23 @@ end
 vim.api.nvim_create_user_command(
   'Profile',
   function(opts)
+    local plenary_profile = require 'plenary.profile'
     if opts.fargs[1] == 'stop' then
       vim.cmd.profile('stop')
-      vim.cmd.tabedit(vim.g.dhleong_last_profile_path)
+      plenary_profile.stop()
+      vim.cmd.tabedit(vim.g.dhleong_last_vim_profile_path)
+      vim.cmd.vsplit(vim.g.dhleong_last_lua_profile_path)
     else
       local path = '~/nvim.profile.log'
-      vim.g.dhleong_last_profile_path = path
+      local lua_path = '~/nvim.lua.profile.log'
+      vim.g.dhleong_last_vim_profile_path = path
+      vim.g.dhleong_last_lua_profile_path = lua_path
       vim.cmd.profile('start', path)
       vim.cmd.profile('func', '*')
       vim.cmd.profile('file', '*')
-      print('Started profiling to:', path)
-      print('Use :Profile stop when done')
+      plenary_profile.start(vim.fn.expand(lua_path))
+      print('Started profiling to:', path, lua_path)
+      print('Use `:Profile stop` when done')
     end
   end,
   {
