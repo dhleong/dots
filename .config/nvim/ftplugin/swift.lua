35852c39c12d6b269438f22decbef951e321efc4
Daniel Leong <me@dhleong.net>
2024-01-10 15:48:02 -0500

    Add a mapping to run a swift project via XCode if it is running

--- .config/nvim/ftplugin/swift.lua
+++ .config/nvim/ftplugin/swift.lua
@@ -1,1 +1,12 @@
 vim.bo.commentstring = '//%s'
+
+vim.keymap.set('n', '<leader>pr', function()
+  vim.fn.system("osascript", [[
+    if application "XCode" is running then
+      tell application "XCode"
+        activate
+        tell application "System Events" to keystroke "r" using {command down}
+      end tell
+    end
+  ]])
+end, { buffer = true })
