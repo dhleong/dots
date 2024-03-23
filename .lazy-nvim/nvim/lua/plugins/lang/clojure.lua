1dd21d58b47104b6f3ed0cc67e6cd84f462a6713
Daniel Leong <me@dhleong.net>
2024-03-23 15:03:57 -0400

    lazy: Restore my clojure plugins

---
+++ .lazy-nvim/nvim/lua/plugins/lang/clojure.lua
@@ -0,0 +1,14 @@
+return {
+
+  { "tpope/vim-fireplace", ft = "clojure" },
+
+  -- Async semantic highlighting (dhleong/vim-mantel)
+  { dir = "~/git/vim-mantel" },
+
+  -- My extra clojure utils (dhleong/vim-hearth)
+  { dir = "~/git/vim-hearth" },
+
+  { "guns/vim-clojure-static", ft = "clojure" },
+  { "guns/vim-sexp", ft = "clojure" },
+  { "tpope/vim-sexp-mappings-for-regular-people", ft = "clojure" },
+}
