4805e61094e1040d4b2ec1a41d7bf10f2c5df3eb
Daniel Leong <me@dhleong.net>
2024-03-23 14:15:18 -0400

    lazy: remap S to also invoke flash
    This is a bit of a crutch, but I can't get a feel for the flash
    treesitter thing so this satisfies my muscle memory from sneak in the
    meantime.

--- .lazy-nvim/nvim/lua/plugins/navigation.lua
+++ .lazy-nvim/nvim/lua/plugins/navigation.lua
@@ -1,17 +1,38 @@
 return {
-  { 'tpope/vim-vinegar', event = 'VeryLazy' },
+  { "tpope/vim-vinegar", event = "VeryLazy" },
   {
-    'junegunn/fzf',
-    build = './install --bin',
-    event = 'VeryLazy'
+    "junegunn/fzf",
+    build = "./install --bin",
+    event = "VeryLazy",
   },
-  { 'junegunn/fzf.vim',  lazy = true },
+  { "junegunn/fzf.vim", lazy = true },
   {
-    dir = '.',
-    event = 'VeryLazy',
-    name = 'My projects',
+    dir = ".",
+    event = "VeryLazy",
+    name = "My projects",
     config = function()
-      require('dhleong.projects').init()
-    end
+      require("dhleong.projects").init()
+    end,
+  },
+
+  {
+    "flash.nvim",
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
+      },
+    },
+    keys = {
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash reverse",
+      },
+    },
   },
 }
