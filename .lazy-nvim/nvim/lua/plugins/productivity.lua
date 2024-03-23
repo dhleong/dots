5f53a109891c2a1b3c88455f7399199eae9732f3
Daniel Leong <me@dhleong.net>
2024-03-23 15:02:05 -0400

    lazy: Restore trot plugin

--- .lazy-nvim/nvim/lua/plugins/productivity.lua
+++ .lazy-nvim/nvim/lua/plugins/productivity.lua
@@ -2,4 +2,17 @@ return {
   { dir = "~/work/otsukare" },
 
   { "tpope/vim-obsession" },
+
+  {
+    "dhleong/trot.nvim",
+    keys = {
+      {
+        "<leader>K",
+        function()
+          require("trot").search()
+        end,
+        desc = "Search in Dash",
+      },
+    },
+  },
 }
