2d821eed31d6ce7e2863f2d192274c42dc5b0296
Daniel Leong <me@dhleong.net>
2024-03-26 23:49:54 +0000

    lazy: Also disable the S mapping in netrw

--- .lazy-nvim/nvim/ftplugin/netrw.lua
+++ .lazy-nvim/nvim/ftplugin/netrw.lua
@@ -1,6 +1,7 @@
--- Disable the 's' mapping in netrw (let us keep using flash/sneak)
+-- Disable the 's' and 'S' mappings in netrw (let us keep using flash/sneak)
 local function disable_maps()
   vim.keymap.del({ "n" }, "s", { buffer = true })
+  vim.keymap.del({ "n" }, "S", { buffer = true })
 end
 
 pcall(disable_maps)
