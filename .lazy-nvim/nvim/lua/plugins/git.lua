34b9c5686eed7f4f92be9492dd2126e5918c054c
Daniel Leong <me@dhleong.net>
2024-03-26 19:48:18 +0000

    lazy: Add a keymap to commit with --no-verify
    Some pre commit hooks are obnoxiously slow...

--- .lazy-nvim/nvim/lua/plugins/git.lua
+++ .lazy-nvim/nvim/lua/plugins/git.lua
@@ -2,6 +2,10 @@ return {
   {
     "tpope/vim-fugitive",
     event = "VeryLazy",
+    keys = {
+      -- "Git no-verify commit"
+      { "<leader>gnc", ":Git commit -a --no-verify<cr>" },
+    },
     config = function()
       vim.cmd.source("~/.vim/init/github.vim")
     end,
