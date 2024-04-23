57905e7eaf1367a0ba8a1928feb4ff438a4157fe
Daniel Leong <me@dhleong.net>
2024-04-23 21:29:02 +0000

    nvim: Enable treesitter textobject movement, but customize mappings
    I'm tempted to move ]c and [c for navigation diagnostics to eg ]d and
    [d, but in the meantime this satisfies my muscle memory while enabling
    the new navigation tools (nice!)

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,17 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      textobjects = {
-        move = {
-          enable = false,
-        },
-      },
-    },
+    opts = function(_, opts)
+      -- Reconfigure the [c and ]c navigation mappings to use
+      -- [k and ]k instead, to avoid conflicts with our diagnostics maps
+      for _, config in pairs(opts.textobjects.move) do
+        if type(config) == "table" then
+          config["]k"] = config["]c"]
+          config["[k"] = config["[c"]
+          config["]c"] = nil
+          config["[c"] = nil
+        end
+      end
+    end,
   },
 }
