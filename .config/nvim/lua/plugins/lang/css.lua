083a925f94911b954a84fdc63988c20674d4b68c
Daniel Leong <me@dhleong.net>
2024-04-24 09:58:04 -0400

    Restrict stylelint_lsp to css/less
    It was very unhappy running in tsx files...

--- .config/nvim/lua/plugins/lang/css.lua
+++ .config/nvim/lua/plugins/lang/css.lua
@@ -6,7 +6,9 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        stylelint_lsp = {},
+        stylelint_lsp = {
+          filetypes = { "css", "less" },
+        },
       },
     },
   },
