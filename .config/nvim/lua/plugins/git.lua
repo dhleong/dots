e56c983604bd4ad6e871b0db3be4a437154c8f92
Daniel Leong <me@dhleong.net>
2024-04-29 14:05:45 -0400

    Add some hacks to ensure we can gq to close GStatus
    Not sure why it keeps breaking, but this'll tide me over...

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,3 +1,28 @@
+local function is_unmapped(keys) -- {{{
+  local maparg = vim.fn.maparg(keys, "n", false, true)
+  return #maparg == 0
+end -- }}}
+
+local function ensure_gq_mapped() -- {{{
+  if is_unmapped("gq") then
+    vim.keymap.set("n", "gq", function()
+      if vim.fn.bufnr("$") == 1 then
+        vim.cmd.quit()
+      else
+        vim.cmd.bdelete()
+      end
+    end, { buffer = true, silent = true })
+  end
+
+  if is_unmapped("q") then
+    vim.keymap.set("n", "q", "gq", {
+      buffer = true,
+      silent = true,
+      remap = true,
+    })
+  end
+end -- }}}
+
 return {
   {
     "tpope/vim-fugitive",
@@ -8,6 +33,14 @@ return {
     },
     config = function()
       vim.cmd.source("~/.vim/init/github.vim")
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "BufReadPost" }, {
+        group = vim.api.nvim_create_augroup("FugitiveStatusFixGroup", {
+          clear = true,
+        }),
+        pattern = "fugitive://*",
+        callback = ensure_gq_mapped,
+      })
 
       -- Having Gbrowse declared but a no-op can make it kinda annoying
       -- to actually get to GBrowse
