db1bbb4c5a5b994d04b0322a68d180431f3cf27b
Daniel Leong <me@dhleong.net>
2024-05-05 17:20:43 -0400

    neovide: Remember window size

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -86,6 +86,7 @@ if vim.g.neovide then
   vim.g.neovide_cursor_vfx_mode = "pixiedust"
   vim.g.neovide_scroll_animation_length = 0.1
   vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_remember_window_size = true
 
   -- It might be possible to make this look nice, but right now it's
   -- just distracting:
