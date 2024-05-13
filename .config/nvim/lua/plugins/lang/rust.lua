e9d5a097bf4d10af80900233be5b3fcf15229d5b
Daniel Leong <me@dhleong.net>
2024-05-12 20:27:17 -0400

    lazy: Import debugger support for rust tests
    Using my old executable selector with the new nvim-dap stuff

--- .config/nvim/lua/plugins/lang/rust.lua
+++ .config/nvim/lua/plugins/lang/rust.lua
@@ -1,4 +1,38 @@
+local function debug_nearest()
+  local helper = require("ft.rust")
+  local module = helper.get_nearest_module()
+
+  print("Compiling " .. module .. "...")
+
+  local path = helper.compile_module_for_executable(module)
+  if not path then
+    print("No executable produced")
+    return
+  end
+
+  -- NOTE: Some projects need the DYLD_LIBRARY_PATH to be set explicitly
+  -- or else they crash when run, complaining about "no LC_RPATH's found"
+  -- for @rpath/libstd-*. Let's go ahead and handle that:
+  local sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+
+  require("dap").run({
+    type = "codelldb",
+    request = "launch",
+    name = "Run nearest",
+    program = path,
+    args = { module },
+    env = {
+      DYLD_LIBRARY_PATH = sysroot .. "/lib",
+    },
+  })
+end
+
 return {
+  { import = "lazyvim.plugins.extras.lang.rust" },
+
+  -- Feels like a bit much to me atm; may try again later:
+  { "rustaceanvim", enable = false },
+
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -31,5 +65,20 @@ return {
         },
       },
     },
+  },
+
+  {
+    "mason-nvim-dap.nvim",
+    keys = {
+      {
+        "<leader>rd",
+        debug_nearest,
+        desc = "Debug Nearest",
+      },
+    },
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "codelldb" })
+    end,
   },
 }
--- .old-nvim/nvim/ftplugin/rust.lua
+++ .old-nvim/nvim/ftplugin/rust.lua
@@ -1,12 +1,14 @@
-local map = require 'helpers.map'
+if not vim.g.lazy_did_setup then
+  local map = require("helpers.map")
 
-require 'dhleong.debugger'.configure {
-  adapter = 'CodeLLDB',
-}
+  require("dhleong.debugger").configure({
+    adapter = "CodeLLDB",
+  })
 
-map.nno '<leader>rd' {
-  lua_module = 'ft.rust',
-  lua_call = 'debug_nearest()',
-}
+  map.nno("<leader>rd")({
+    lua_module = "ft.rust",
+    lua_call = "debug_nearest()",
+  })
 
-vim.cmd([[command! Debug lua require'ft.rust'.debug_app()]])
+  vim.cmd([[command! Debug lua require'ft.rust'.debug_app()]])
+end
