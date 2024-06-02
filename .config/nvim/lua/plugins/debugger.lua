0d24c28560d6167461377c28e55725d21544c6d9
Daniel Leong <me@dhleong.net>
2024-06-02 13:33:04 -0700

    Install my nvim-dap-mappings plugin to restore old debugger maps
    Turned my old util into a sorta proper plugin for use with nvim-dap!

--- .config/nvim/lua/plugins/debugger.lua
+++ .config/nvim/lua/plugins/debugger.lua
@@ -1,4 +1,25 @@
 return {
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.dap.nlua" },
+
+  {
+    "nvim-dap",
+    dependencies = {
+      {
+        "dhleong/nvim-dap-mappings",
+        dev = true,
+        opts = {
+          keymaps = {
+            { "gk", dap = "run_to_cursor" },
+            { "gi", dap = "step_into" },
+            { "gn", dap = "step_over" },
+            { "go", dap = "step_out" },
+            { "gp", dap = "pause" },
+            { "gr", dap = "continue" }, -- "resume"
+            { "gq", dap = "terminate" }, -- "quit"
+          },
+        },
+      },
+    },
+  },
 }
