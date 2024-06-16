0158b081f0f69f83571484634df86909a99fcb39
Daniel Leong <me@dhleong.net>
2024-06-16 09:31:31 -0400

    Restore rhubarb, and commit to using lilium's LSP mode

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -25,44 +25,31 @@ end -- }}}
 
 return {
   {
-    "tpope/vim-fugitive",
+    "tpope/vim-rhubarb",
     event = "VeryLazy",
-    keys = {
-      -- "Git no-verify commit"
-      { "<leader>gnc", ":Git commit -a --no-verify<cr>" },
+    dependencies = {
+      {
+        "tpope/vim-fugitive",
+        keys = {
+          -- "Git no-verify commit"
+          { "<leader>gnc", ":Git commit -a --no-verify<cr>" },
+        },
+        config = function()
+          vim.cmd.source("~/.vim/init/github.vim")
+
+          -- Having Gbrowse declared but a no-op can make it kinda annoying
+          -- to actually get to GBrowse
+          pcall(vim.api.nvim_del_user_command, "Gbrowse")
+        end,
+      },
     },
-    config = function()
-      vim.cmd.source("~/.vim/init/github.vim")
-
-      -- Having Gbrowse declared but a no-op can make it kinda annoying
-      -- to actually get to GBrowse
-      pcall(vim.api.nvim_del_user_command, "Gbrowse")
-    end,
   },
-
-  -- {
-  --   "dhleong/lilium",
-  --   dev = true,
-  --   ft = "gitcommit",
-  --   lazy = true,
-  --   event = "VeryLazy",
-  --   cmd = {
-  --     "LiliumInfo",
-  --     -- "GBrowse",  -- Sadly, doing this doesn't work
-  --   },
-  --   dependencies = {
-  --     "tpope/vim-fugitive",
-  --   },
-  --   opts = {},
-  --   init = function()
-  --     require("lilium.lsp").setup()
-  --   end,
-  -- },
 
   {
     "dhleong/lilium",
     dev = true,
-    lazy = false,
+    event = "VeryLazy",
+    build = "cargo build",
     opts = {
       -- NOTE: It'd be nice if we could use the normal lazy lsp init
       -- but for some reason that doesn't work...?
