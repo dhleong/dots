ee8a846bead36c5c7ffea63f5cd00ac2ec7a011c
Daniel Leong <me@dhleong.net>
2025-04-23 17:18:14 +0000

    Restore old behavior of ]c for jumping to diff conflict markers

--- .config/nvim/lua/plugins/lsp/keymaps.lua
+++ .config/nvim/lua/plugins/lsp/keymaps.lua
@@ -2,11 +2,28 @@ local M = {}
 
 local function diagnostic_jump(count)
   return function()
+    if vim.wo.diff then
+      local flags = ""
+      if count < 0 then
+        flags = flags .. "b"
+      end
+      local line = vim.fn.search("^<<<<<<<", flags)
+      if line ~= 0 then
+        -- Found something!
+        return
+      end
+
+      print("No more conflicts found!")
+      -- NOTE: fall through to diagnostic jump
+    end
+
     if vim.diagnostic.jump then
       vim.diagnostic.jump({ count = count, float = true })
     elseif count < 0 then
+      ---@diagnostic disable-next-line: deprecated
       vim.diagnostic.goto_prev()
     else
+      ---@diagnostic disable-next-line: deprecated
       vim.diagnostic.goto_next()
     end
   end
