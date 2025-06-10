48c7bb9099838ca0e1f250b4dae9e78db77907c2
Daniel Leong <me@dhleong.net>
2025-06-10 14:39:21 -0400

    ai: Add basic integration with jean.nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -2,6 +2,26 @@ local AI_PLUGIN = "codecompanion"
 
 return {
   {
+    dev = true,
+    keys = {
+      {
+        "g/",
+        function()
+          require("jean").prompt()
+        end,
+      },
+      {
+        "<leader>ctd",
+        function()
+          require("jean").submit("Implement the TODO or FIXME closest to the current line")
+        end,
+      },
+    },
+    "dhleong/jean.nvim",
+    opts = {},
+  },
+
+  {
     import = "plugins.ai.avante",
     enabled = AI_PLUGIN == "avante",
   },
